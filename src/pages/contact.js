5e5d5138d03bdc09860018072cdd33651a93395c
Muhammad Masham <66207787+masham04@users.noreply.github.com>
2021-05-07 23:43:37 +0500

    new

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>Muhammad Masham</h1>
+        </div>
+    )
+}
+
+export default contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import './blogs.css'
+import logo from '../images/BlogPost.jpg'
 
 const IndexPage = () => {
   return (
-    <>
-      <h1>BLOGSITE</h1>
+    <div>
+      {/*
       <div className='btn'>
         <Link to="/blogs"><button className='btn1'>Blog Page</button></Link>
-      </div>
-
-      <div className='image'>
-      </div>
-    </>
+      </div> */}
+      <ul>
+        <li style={{ float: 'left' }}><Link id='a' to="/"><h2>BLOGSITE</h2></Link></li>
+        <li><Link id='a' to="/contact">Contact</Link></li>
+        <li><Link id='a' to="/blogs">Blogs</Link></li>
+      </ul>
+      <img src={logo} alt='logo' className='bg-img' />
+    </div>
   )
 }
 
